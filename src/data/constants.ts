467746edb44544a14f23a4a6e5b67e017233723b
MarcPG <me@marcpg.com>
2026-05-24 14:58:11 +0200

    Basic Assets

---
+++ src/data/constants.ts
@@ -0,0 +1,8 @@
+export const gameName = "DrachenClicker"
+export const unit = "Ofenkäse"
+export const unitPerSecond = "OK/s"
+
+export const costGrowthFactor = 1.15
+export const baseClickFactor = 0.10 // 10% of OK/s
+export const wealthUpdateInterval = 0.05 // 50 milliseconds / 20 UPS
+export const autosaveInterval = 60 // 60 seconds
